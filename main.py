e6aae6a9899cdc9f36cdffbb85e57cd05b5172c0
yurazver <yurazver0@gmail.com>
2024-08-25 18:33:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
 first_result = [len(x) for x in first_strings if len(x) >= 5]
-second_result = []
-for word1 in first_strings:
-    for word2 in second_strings:
-        if len(word1) == len(word2):
-            second_result.append((word1, word2))
+second_result = [(s1, s2) for s1 in first_strings for s2 in second_strings if len(s1) == len(s2)]
+
 
 third_result = {string: len(string) for string in first_strings + second_strings if len(string) % 2 == 0}
 
